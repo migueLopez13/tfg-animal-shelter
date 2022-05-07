93b03e3b4237203a931a0860e4318af69a2f601b
Miguel Ángel López <miguel.lopez@lambdamotive.com>
2022-05-07 20:56:31 +0200

    feat(backend): animals controller skeleton

--- animal-shelter-back/src/modules/animals/animals.controller.ts
+++ animal-shelter-back/src/modules/animals/animals.controller.ts
@@ -1,9 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get, 
+  Post, 
+  Put, 
+  Delete, 
+  Body, 
+  Param, 
+  ValidationPipe 
+} from '@nestjs/common';
+import { AnimalDTO } from 'src/shared/domain/dto/animal.dto';
+import { AnimalsService } from './animasl.service';
 
 @Controller('animals')
 export class AnimalsController {
+  constructor(private readonly animalsService: AnimalsService) { }
+
   @Get()
-  private helloWorld() {
-    return 'Hello World!';
+  private find() {
+    return 'return all animals';
+  }
+
+  @Get('id')
+  private findOne(@Param('id') params) {
+    return "return animal with this id";
+  }
+
+  @Post()
+  private insert(@Body(ValidationPipe) animal: AnimalDTO) {
+    return "add animal";
+  }
+
+  @Put()
+  private update(@Body(ValidationPipe) animal: AnimalDTO) {
+    return "update animal";
+  }
+
+  @Delete(':id')
+  private delete(@Param('id') params) {
+    return "delete animal";
   }
 }
