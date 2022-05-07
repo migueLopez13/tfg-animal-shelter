9ff73f0bcc68e0809af0c3fd61305215ed4a433d
Miguel Ángel López <miguel.lopez@lambdamotive.com>
2022-05-07 20:52:01 +0200

    feat(backend): adoptions controller skeleton

--- animal-shelter-back/src/modules/adoptions/adoptions.controller.ts
+++ animal-shelter-back/src/modules/adoptions/adoptions.controller.ts
@@ -1,9 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get, 
+  Post, 
+  Put, 
+  Delete, 
+  Body, 
+  Param, 
+  ValidationPipe 
+} from '@nestjs/common';
+import { AdoptionDTO } from 'src/shared/domain/dto/adoption.dto';
+import { AdoptionsService } from './adoptions.service';
 
 @Controller('adoptions')
 export class AdoptionsController {
+  constructor(private readonly adoptionsService: AdoptionsService) { }
+
   @Get()
-  private helloWorld() {
-    return 'Hello World!';
+  private find() {
+    return 'return all adoptions';
+  }
+
+  @Get('id')
+  private findOne(@Param('id') params) {
+    return "return adoption with this id";
+  }
+
+  @Post()
+  private insert(@Body(ValidationPipe) adoption: AdoptionDTO) {
+    return "add adoption";
+  }
+
+  @Put()
+  private update(@Body(ValidationPipe) adoption: AdoptionDTO) {
+    return "update adoption";
+  }
+
+  @Delete(':id')
+  private delete(@Param('id') params) {
+    return "delete adoption";
   }
 }
