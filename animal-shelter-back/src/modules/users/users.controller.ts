a1f86029f5896b6810d0913e8738807d47ac1d4d
Miguel Ángel López <miguel.lopez@lambdamotive.com>
2022-05-07 20:56:52 +0200

    feat(backend): users controller skeleton

--- animal-shelter-back/src/modules/users/users.controller.ts
+++ animal-shelter-back/src/modules/users/users.controller.ts
@@ -1,9 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get, 
+  Post, 
+  Put, 
+  Delete, 
+  Body, 
+  Param, 
+  ValidationPipe 
+} from '@nestjs/common';
+import { UserDTO } from 'src/shared/domain/dto/user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
   @Get()
-  private helloWorld() {
-    return 'Hello World!';
+  private find() {
+    return 'return all users';
+  }
+
+  @Get('id')
+  private findOne(@Param('id') params) {
+    return "return shelter with this id";
+  }
+
+  @Post()
+  private insert(@Body(ValidationPipe) user: UserDTO) {
+    return "add user";
+  }
+
+  @Put()
+  private update(@Body(ValidationPipe) user: UserDTO) {
+    return "update user";
+  }
+
+  @Delete(':id')
+  private delete(@Param('id') params) {
+    return "delete user";
   }
 }
