a5a66c6648e352d0635e1c2f576c7a0c647fd06d
Miguel Ángel López <miguel.lopez@lambdamotive.com>
2022-05-07 19:47:26 +0200

    feat(backend): add typeorm config && backend/.env

--- animal-shelter-back/src/app.module.ts
+++ animal-shelter-back/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ animal-shelter-back/src/shared/database/entities.ts
@@ -0,0 +1,3 @@
+export const entities = [];
+
+export default entities;
