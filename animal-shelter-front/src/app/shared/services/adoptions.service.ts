72f5a1752a751084f92c45e1f34e17facf772b13
Miguel Ángel López <miguel.lopez@lambdamotive.com>
2022-06-04 18:06:24 +0200

    feat(front): build base service and implements

--- animal-shelter-front/src/app/shared/services/adoptions.service.ts
+++ animal-shelter-front/src/app/shared/services/adoptions.service.ts
@@ -1,26 +1,13 @@
 import { Injectable } from "@angular/core";
 import { Adoption } from "src/app/common/interfaces/adoption.interface";
 import { AdoptionsRepository } from "../repository/adoptions.repository";
+import { BaseService } from "./base.service";
 
 @Injectable({
   providedIn: 'root',
 })
-export class AdoptionsService {
-  constructor(private readonly adoptionsRepository: AdoptionsRepository) { }
-
-  findAll() {
-    return this.adoptionsRepository.findAll()
-  }
-
-  find(id: number) {
-    return this.adoptionsRepository.findOne(id)
-  }
-
-  add(adoption: Adoption) {
-    return this.adoptionsRepository.save(adoption)
-  }
-
-  remove(id: number) {
-    return this.adoptionsRepository.delete(id)
+export class AdoptionsService extends BaseService<Adoption> {
+  constructor(private readonly adoptionsRepository: AdoptionsRepository) {
+    super(adoptionsRepository)
   }
 }
---
+++ animal-shelter-front/src/app/shared/services/base.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { AdoptionsRepository } from "../repository/adoptions.repository";
+import { BaseRepository } from "../repository/base.repository";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BaseService<T> {
+  constructor(private readonly baseRepository: BaseRepository<T>) { }
+
+  findAll() {
+    return this.baseRepository.findAll()
+  }
+
+  find(id: number) {
+    return this.baseRepository.findOne(id)
+  }
+
+  add(t: T) {
+    return this.baseRepository.save(t)
+  }
+
+  remove(id: number) {
+    return this.baseRepository.delete(id)
+  }
+}
